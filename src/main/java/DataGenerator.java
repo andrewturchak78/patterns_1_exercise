a53815ea4def2bdc943438d3233f5fa044b67a40
Andrey Turchak <andrewturchak78@mail.ru>
2023-04-02 20:32:11 +0300

    test fixed again

--- src/main/java/DataGenerator.java
+++ src/main/java/DataGenerator.java
@@ -2,7 +2,6 @@
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
--- src/test/java/DeliveryCardTest.java
+++ src/test/java/DeliveryCardTest.java
@@ -15,21 +15,22 @@ import static com.codeborne.selenide.Selenide.*;
 public class DeliveryCardTest {
     DataGenerator generator = new DataGenerator();
     private static Faker faker;
-    @BeforeAll
-    static void setUpAll() {
+
+    public static String generateName() {
         faker = new Faker(new Locale("ru"));
+        return faker.name().fullName();
     }
-    String name = faker.name().fullName();
-    String phone = faker.phoneNumber().phoneNumber();
-    @BeforeEach
-    void setUpConnection() {
-        open("http://localhost:9999/");
+
+    public static String generatePhone() {
+        faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
     }
 
     @Test
     void shouldCardTest() {
-        $("[data-test-id=name] input").sendKeys(name);
-        $("[data-test-id=phone] input").sendKeys(phone);
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input").sendKeys(generateName());
+        $("[data-test-id=phone] input").sendKeys(generatePhone());
         $("[data-test-id=city] input").sendKeys(generator.generateCity());
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] input").sendKeys(generator.generateDate(5));
@@ -39,7 +40,7 @@ public class DeliveryCardTest {
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] input").sendKeys(generator.generateDate(7));
         $$("button").find(exactText("Запланировать")).click();
-        $("[data-test-id='replan-notification']").shouldBe(visible,Duration.ofSeconds(5));
+        $("[data-test-id='replan-notification']").shouldBe(visible, Duration.ofSeconds(5));
         $$("button").find(exactText("Перепланировать")).click();
         $("[data-test-id='success-notification'] .notification__content").shouldHave(exactText("Встреча успешно запланирована на " + generator.generateDate(7)));
     }
